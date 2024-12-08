1ba3b73df9dd5f084a43e16fb72dc35bc3bd7f4c
shafwanhasyim <shafwan178@gmail.com>
2024-12-08 14:05:53 +0700

    GYATTT

--- cipher_combined.vhd
+++ cipher_combined.vhd
@@ -6,7 +6,7 @@ entity cipher_combined is
     Port ( 
         clk : in STD_LOGIC;
         reset : in STD_LOGIC;
-        mode : in STD_LOGIC;
+        mode : in STD_LOGIC_VECTOR(1 downto 0);  -- "00": Case1-Encrypt, "01": Case1-Decrypt, "10": Case2-Encrypt, "11": Case2-Decrypt
         start : in STD_LOGIC;
         shift_value : in STD_LOGIC_VECTOR(4 downto 0);
         input_char : in STD_LOGIC_VECTOR(7 downto 0);
@@ -17,8 +17,10 @@ end cipher_combined;
 
 architecture Behavioral of cipher_combined is
     type state_type is (IDLE, 
-                       CAESAR_ENC, HILL_ENC, HILL_DEC, CAESAR_DEC,
-                       HILL_ENC2, CAESAR_ENC2, CAESAR_DEC2, HILL_DEC2);
+                       CAESAR_ENC1, HILL_ENC1,   -- Case 1 Encrypt
+                       HILL_DEC1, CAESAR_DEC1,   -- Case 1 Decrypt
+                       HILL_ENC2, CAESAR_ENC2,   -- Case 2 Encrypt
+                       CAESAR_DEC2, HILL_DEC2);  -- Case 2 Decrypt
     signal current_state, next_state : state_type;
     
     signal caesar_mode, hill_mode : STD_LOGIC;
@@ -77,42 +79,27 @@ begin
             case current_state is
                 when IDLE =>
                     if start = '1' then
-                        if mode = '0' then
-                            next_state <= CAESAR_ENC;
-                        else
-                            next_state <= HILL_ENC2;
-                        end if;
+                        case mode is
+                            when "00" => next_state <= CAESAR_ENC1;  -- Case 1 Encrypt
+                            when "01" => next_state <= HILL_DEC1;    -- Case 1 Decrypt
+                            when "10" => next_state <= HILL_ENC2;    -- Case 2 Encrypt
+                            when "11" => next_state <= CAESAR_DEC2;  -- Case 2 Decrypt
+                            when others => next_state <= IDLE;
+                        end case;
                         state_delay <= 0;
                     end if;
 
-                when CAESAR_ENC =>
+                -- Case 1 Encrypt States
+                when CAESAR_ENC1 =>
                     if state_delay = 2 then
                         intermediate_data <= caesar_enc_out;
-                        next_state <= HILL_ENC;
+                        next_state <= HILL_ENC1;
                         state_delay <= 0;
                     else
                         state_delay <= state_delay + 1;
                     end if;
 
-                when HILL_ENC =>
-                    if state_delay = 2 then
-                        intermediate_data <= hill_enc_out;
-                        next_state <= HILL_DEC;
-                        state_delay <= 0;
-                    else
-                        state_delay <= state_delay + 1;
-                    end if;
-
-                when HILL_DEC =>
-                    if state_delay = 2 then
-                        intermediate_data <= hill_enc_out;
-                        next_state <= CAESAR_DEC;
-                        state_delay <= 0;
-                    else
-                        state_delay <= state_delay + 1;
-                    end if;
-
-                when CAESAR_DEC =>
+                when HILL_ENC1 =>
                     if state_delay = 2 then
                         next_state <= IDLE;
                         state_delay <= 0;
@@ -120,6 +107,25 @@ begin
                         state_delay <= state_delay + 1;
                     end if;
 
+                -- Case 1 Decrypt States
+                when HILL_DEC1 =>
+                    if state_delay = 2 then
+                        intermediate_data <= hill_enc_out;
+                        next_state <= CAESAR_DEC1;
+                        state_delay <= 0;
+                    else
+                        state_delay <= state_delay + 1;
+                    end if;
+
+                when CAESAR_DEC1 =>
+                    if state_delay = 2 then
+                        next_state <= IDLE;
+                        state_delay <= 0;
+                    else
+                        state_delay <= state_delay + 1;
+                    end if;
+
+                -- Case 2 Encrypt States
                 when HILL_ENC2 =>
                     if state_delay = 2 then
                         intermediate_data <= hill_enc_out;
@@ -131,13 +137,13 @@ begin
 
                 when CAESAR_ENC2 =>
                     if state_delay = 2 then
-                        intermediate_data <= caesar_enc_out;
-                        next_state <= CAESAR_DEC2;
+                        next_state <= IDLE;
                         state_delay <= 0;
                     else
                         state_delay <= state_delay + 1;
                     end if;
 
+                -- Case 2 Decrypt States
                 when CAESAR_DEC2 =>
                     if state_delay = 2 then
                         intermediate_data <= caesar_enc_out;
@@ -163,20 +169,20 @@ begin
     process(current_state, input_char, intermediate_data)
     begin
         case current_state is
-            when CAESAR_ENC =>
+            when CAESAR_ENC1 =>
                 caesar_input <= input_char;
-            when HILL_ENC =>
+            when HILL_ENC1 =>
                 hill_input <= intermediate_data;
-            when HILL_DEC =>
-                hill_input <= intermediate_data;
-            when CAESAR_DEC =>
+            when HILL_DEC1 =>
+                hill_input <= input_char;
+            when CAESAR_DEC1 =>
                 caesar_input <= intermediate_data;
             when HILL_ENC2 =>
                 hill_input <= input_char;
             when CAESAR_ENC2 =>
                 caesar_input <= intermediate_data;
             when CAESAR_DEC2 =>
-                caesar_input <= intermediate_data;
+                caesar_input <= input_char;
             when HILL_DEC2 =>
                 hill_input <= intermediate_data;
             when others =>
@@ -189,17 +195,17 @@ begin
     process(current_state)
     begin
         case current_state is
-            when CAESAR_ENC | CAESAR_ENC2 =>
-                caesar_mode <= '0';
+            when CAESAR_ENC1 | CAESAR_ENC2 =>
+                caesar_mode <= '0';  -- Encrypt
                 hill_mode <= '0';
-            when HILL_ENC | HILL_ENC2 =>
+            when HILL_ENC1 | HILL_ENC2 =>
                 caesar_mode <= '0';
-                hill_mode <= '0';
-            when HILL_DEC | HILL_DEC2 =>
+                hill_mode <= '0';    -- Encrypt
+            when HILL_DEC1 | HILL_DEC2 =>
                 caesar_mode <= '0';
-                hill_mode <= '1';
-            when CAESAR_DEC | CAESAR_DEC2 =>
-                caesar_mode <= '1';
+                hill_mode <= '1';    -- Decrypt
+            when CAESAR_DEC1 | CAESAR_DEC2 =>
+                caesar_mode <= '1';  -- Decrypt
                 hill_mode <= '0';
             when others =>
                 caesar_mode <= '0';
@@ -211,18 +217,18 @@ begin
     process(current_state, caesar_enc_out, hill_enc_out)
     begin
         case current_state is
-            when CAESAR_ENC | CAESAR_ENC2 =>
+            when CAESAR_ENC1 | CAESAR_ENC2 =>
                 output_char <= caesar_enc_out;
                 done <= '0';
-            when HILL_ENC | HILL_ENC2 =>
+            when HILL_ENC1 | HILL_ENC2 =>
+                output_char <= hill_enc_out;
+                done <= '1';  -- Done for encryption
+            when HILL_DEC1 | HILL_DEC2 =>
                 output_char <= hill_enc_out;
                 done <= '0';
-            when HILL_DEC | HILL_DEC2 =>
-                output_char <= hill_enc_out;
-                done <= '0';
-            when CAESAR_DEC | CAESAR_DEC2 =>
+            when CAESAR_DEC1 | CAESAR_DEC2 =>
                 output_char <= caesar_enc_out;
-                done <= '1';
+                done <= '1';  -- Done for decryption
             when others =>
                 output_char <= (others => '0');
                 done <= '0';
